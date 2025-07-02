60b24353b74a7554c438c393e12a9d41a20aa267
Minsung Kang <kmins97@naver.com>
2025-07-02 15:43:39 +0900

    추가

--- src/components/Condition List.js
+++ src/components/Condition List.js
@@ -13,8 +13,7 @@ export default function ConditionList({ conditions, onConditionClick, warning })
             className="condition-item"
             onClick={() => onConditionClick(cond)}
           >
-            {/* 기존 cond.name 대신 type > path : detail로 변경 */}
-            {cond.type || "기타"} > {cond.path || "(경로 없음)"} : {cond.detail || "(내용 없음)"}
+            {cond.type || "기타"}{">"}{cond.path || "(경로 없음)"} : {cond.detail || ""}
           </div>
         ))}
       </div>
